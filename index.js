8e817f94ecff4b39686eff89ccadc7e26e683261
CompSciHigh24 <csh-github@compscihigh.org>
2025-01-13 04:54:26 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+// Step 1: Create a "public" folder
+
+// Step 2: In this directory move your static files like HTML, CSS, and JavaScript. (Not your index.js, this is your express file.)
+
+// Step 3: Set up an Express.js server.
+// Import the Express module.
+// Create an Express application.
+
+
+
+
+
+// Step 4: Set Up Middleware for Logging
+// Console log the method and path of every request. 
+
+
+
+
+
+// Step 5: Serve Static Files
+// Remeber to use the variable __dirname
+
+
+
+
+
+// Step 6: Create a route handler for the home route
+// Once inside sent the file index.html
+
+
+
+
+
+// Step 6: Start the Server and run your application. 
+
+
+
+
